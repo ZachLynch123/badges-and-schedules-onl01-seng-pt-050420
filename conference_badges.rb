387a84aabf85cecb511cbde6912c904b5064f4cd
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-07 17:59:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,11 @@
 speakers = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
-list_of_names = []
-rooms = []
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
 def batch_badge_crator(array)
+  list_of_names = []
   array.each do |name|
     list_of_names << badge_maker(name)
   end
